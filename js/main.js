c20064cf8859bb894d1f2f74e200bb0eb4c7ee4f
andycanderson <chen.a.andy@gmail.com>
2014-08-18 16:11:20 -0700

    added counter frame

--- js/main.js
+++ js/main.js
@@ -52,50 +52,100 @@ function get_image(x){
 	return image;
 
 }
+function get_count_down_image(x){
+	var image;
+	switch (x){
+		case 3:
+			image = "http://goo.gl/lmdIz7"
+			break;
+		case 2:
+			image = "http://goo.gl/BK0dP5"
+			break;
+		default:
+			image = "http://goo.gl/IY1Lf7"
+	}
+	return image;
+}
+
+function set_counter(i, user){
+	document.getElementById("count_down").innerHTML = i;
+	document.getElementById("loading").style.backgroundImage = "url(" + get_count_down_image(i) + ")";
+			
+
+    if (i > 0) {
+		window.setTimeout(function(){set_counter(i-1, user);}, 1000);
+
+	}
+	else if (i === 0)
+	{
+		document.getElementById("count_down_container").style.display = "none";
+		set_results(user);
+	}
+}
+
+function timer_window(user){
+	document.getElementById("count_down_container").style.display = "block";
+	document.getElementById("hide_selection_block").style.display = "none";
+	set_counter(3, user);
 
 
+	// ALTERNATE WAY TO COUNTDOWN
+	// for (i = 3; i>0; i--)
+	// {
+	// 	window.setTimeout(function(){set_counter(i)}, 1000*i);
+	// }
+	// window.setTimeout(function(){set_counter(3)}, 1000);
+	// window.setTimeout(function(){set_counter(2)}, 2000);
+	// window.setTimeout(function(){set_counter(1)}, 3000);
+
+}
+
+
+
+function set_results (user){
+		var computer = computerchoice();
+		document.getElementById("human").style.backgroundImage = "url(" + get_image(user) + ")";
+		document.getElementById("computer").style.backgroundImage = "url(" + get_image(computer) + ")";
+		document.getElementById("result").innerHTML = compare(user, computer);
+		document.getElementById("play_again_box").style.display = "block";
+}
 
 
 
 
 function startup (){
+	document.getElementById("select_weapon").onclick = function(){
+	document.getElementById("selection_block").style.opacity = ".2";
+	window.setTimeout(function(){
+		document.getElementById("selection_block").style.opacity = "1";
+	}, 100);
+
+	}
 	document.getElementById("play_again_box").style.display = "none";
 	document.getElementById("hide_selection_block").style.display = "block";
+	document.getElementById("count_down_container").style.display = "none";
+	
 	document.getElementById("rock").onclick = function(){
-		var computer = computerchoice();
-		document.getElementById("human").style.backgroundImage = "url(" + get_image(1) + ")";
-		document.getElementById("computer").style.backgroundImage = "url(" + get_image(computer) + ")";
-		document.getElementById("result").innerHTML = compare(1, computer);
-		document.getElementById("hide_selection_block").style.display = "none";
-		document.getElementById("play_again_box").style.display = "block";
+		timer_window(1);		
 	}
 	document.getElementById("paper").onclick = function(){
-		var computer = computerchoice();
-		console.log(compare(2, computer));
-		document.getElementById("human").style.backgroundImage = "url(" + get_image(2) + ")";
-		document.getElementById("computer").style.backgroundImage = "url(" + get_image(computer) + ")";
-		document.getElementById("result").innerHTML = compare(2, computer);
-		document.getElementById("result_box").style.display= "block";
-		document.getElementById("hide_selection_block").style.display = "none";
-		document.getElementById("play_again_box").style.display = "block";
+		timer_window(2);
 	}
 	document.getElementById("scissors").onclick = function(){
-		var computer = computerchoice();
-		console.log(compare(3, computer));
-		document.getElementById("human").style.backgroundImage = "url(" + get_image(3) + ")";
-		document.getElementById("computer").style.backgroundImage = "url(" + get_image(computer) + ")";
-		document.getElementById("result").innerHTML = compare(3, computer);
-		document.getElementById("result_box").style.display= "block";
-		document.getElementById("hide_selection_block").style.display = "none";
-		document.getElementById("play_again_box").style.display = "block";
+		timer_window(3);
 	}
 }
 
 document.getElementById("play_again").onclick = function (){
+	//WAY TO REFRESH WINDOW
 	//window.location.reload();
 	startup();
 }
+
+
 window.onload = startup;
+
+
 
 
 
